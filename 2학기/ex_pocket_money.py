68778a8938c0bdcafbaefca25d8254ca01c4c7f5
chaerin <cpt0202@naver.com>
2019-09-03 12:05:20 +0900

    add ex_pocket_money

---
+++ 2학기/ex_pocket_money.py
@@ -0,0 +1,9 @@
+# 국어, 영어, 수학, 자바, 파이썬, JSP 점수 입력받기
+Korea = int(input("국어 점수를 입력받으세요 : "))
+English = int(input("영어 점수를 입력받으세요 : "))
+Math = int(input("수학 점수를 입력받으세요 : "))
+Java = int(input("자바 점수를 입력받으세요 : "))
+Python = int(input("파이썬 점수를 입력받으세요 : "))
+JSP = int(input("JSP 점수를 입력받으세요 : "))
+
+# 총점, 평균 구하기
