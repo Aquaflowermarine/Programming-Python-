3d7b1f05b6cb3a40b6859cb44bbbfb8f2a110df4
chaerin <cpt0202@naver.com>
2019-11-25 17:25:29 +0900

    add crawling - 신의 탑

--- 2학기/IX. 프로젝트/web_crawling.py
+++ 2학기/IX. 프로젝트/web_crawling.py
@@ -5,18 +5,19 @@ from bs4 import BeautifulSoup  # html 구조적으로 변환하자   alt + shift
 from urllib.request import urlopen
 
 if __name__ == '__main__':
-    # # 네이버 웹툰 > 신의 탑 제목 가져오자
-    # data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=183559")
-    # soup = BeautifulSoup(data, "lxml")
-    # # print(soup)
-    # cartoon_titles = soup.find_all("td", attrs={'class':'title'})  # class 가 title 인 것만 출력
-    # for title in cartoon_titles:
-    #     t = title.find('a').text
-    #     print(t)
+     # 네이버 웹툰 > 신의 탑 제목 가져오자
+     data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=183559")
+     soup = BeautifulSoup(data, "lxml")
+     cartoon_titles = soup.find_all("td", attrs={"class":"title"})  # class 가 title 인 것만 출력
+     html = "<html><body>"
+     for title in cartoon_titles:
+         t = title.find("a").text # 제목 가져오자
+         link = title.find("a").get("href") # 링크 가져오자
+         link = "http://comic.naver.com/" + link
+         print(t)
+         print(link)
+         print("<a href='"+link+"'>"+t+"</a>")
+         html += "<a href='"+link+"'>"+t+"</a>"
+     html += "</body></html>"
+     print(html)
 
-    # 다음 웹툰 > 어쩌다 발견한 7월 제목 가져오자
-    data = urlopen("http://webtoon.daum.net/webtoon/view/findjuly") # url -> httpResponse 객체
-    soup = BeautifulSoup(data, "lxml")
-    cartoon_titles = soup.find_all("string", attrs={'class':'tit_wt'})
-    for title in cartoon_titles:
-        print(title)
